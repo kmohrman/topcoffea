978e4f1cf01ad44aa6ba7cc5a0e971573bd20b17
Kelci <k.mohrman@ufl.edu>
2023-08-20 00:17:52 -0400

    Dump to latex table

--- analysis/wwz/get_wwz_yields.py
+++ analysis/wwz/get_wwz_yields.py
@@ -2,6 +2,8 @@ import argparse
 import pickle
 import gzip
 
+import topcoffea.modules.MakeLatexTable as mlt
+
 # This script opens a pkl file of histograms produced by wwz processor
 # Reads the histograms and dumps out the yields for each group of processes
 # Example usage: python get_yld_check.py -f histos/tmp_histo.pkl.gz
@@ -39,10 +41,42 @@ sample_dict_base = {
         ##"WJetsToLNu",
         "WWTo2L2Nu",
         "WZTo3LNu",
-    ]
-        
+    ],
+
+    "WWZJetsTo4L2Nu":            ["WWZJetsTo4L2Nu"],
+    "GluGluZH":                  ["GluGluZH"],
+    "ggToZHToZTo2L":             ["ggToZHToZTo2L"],
+    "ZZTo4l":                    ["ZZTo4l"],
+    "ggToZZTo2e2mu":             ["ggToZZTo2e2mu"], 
+    "ggToZZTo2e2tau":            ["ggToZZTo2e2tau"], 
+    "ggToZZTo2mu2tau":           ["ggToZZTo2mu2tau"], 
+    "ggToZZTo4e":                ["ggToZZTo4e"], 
+    "ggToZZTo4mu":               ["ggToZZTo4mu"], 
+    "ggToZZTo4tau":              ["ggToZZTo4tau"],
+    "TTZToLL_M_1to10":           ["TTZToLL_M_1to10"],
+    "TTZToLLNuNu_M_10":          ["TTZToLLNuNu_M_10"],
+    "TTZToQQ":                   ["TTZToQQ"],
+    ##"DYJetsToLL_M_10to50_MLM": ["DYJetsToLL_M_10to50_MLM"],
+    "DYJetsToLL_M_50_MLM":       ["DYJetsToLL_M_50_MLM"],
+    "SSWW":                      ["SSWW"],
+    "ST_antitop_t-channel":      ["ST_antitop_t-channel"],
+    "ST_top_s-channel":          ["ST_top_s-channel"],
+    "ST_top_t-channel":          ["ST_top_t-channel"],
+    "tbarW_noFullHad":           ["tbarW_noFullHad"],
+    "ttHnobb":                   ["ttHnobb"],
+    "TTTo2L2Nu":                 ["TTTo2L2Nu"],
+    "TTWJetsToLNu":              ["TTWJetsToLNu"],
+    "TTWJetsToQQ":               ["TTWJetsToQQ"],
+    "tW_noFullHad":              ["tW_noFullHad"],
+    "tZq":                       ["tZq"],
+    "VHnobb":                    ["VHnobb"],
+    ##"WJetsToLNu":              ["WJetsToLNu"],
+    "WWTo2L2Nu":                 ["WWTo2L2Nu"],
+    "WZTo3LNu":                  ["WZTo3LNu"],
 
 }
+
+
 
 # Pass dictionary with the base names for the samples, and return with full list for 4 years
 def create_full_sample_dict(in_dict):
@@ -75,11 +109,31 @@ def get_yields(histos_dict):
             val = sum(sum(histos_dict[dense_axis][{"category":cat_name,"process":sample_dict[proc_name]}].values(flow=True)))
             yld_dict[proc_name][cat_name] = val
 
+
+    # Print to screen
     for proc in yld_dict.keys():
         print(f"\n{proc}:")
         for cat in yld_dict[proc].keys():
             val = yld_dict[proc][cat]
             print(f"\t{cat}: {val}")
+
+    # Dump to latex table
+    yld_dict_for_printing = {}
+    cats_to_print = [ "4l_wwz_sf_A", "4l_wwz_sf_B", "4l_wwz_sf_C", "4l_wwz_of_1", "4l_wwz_of_2", "4l_wwz_of_3", "4l_wwz_of_4"]
+    for proc in yld_dict.keys():
+        yld_dict_for_printing[proc] = {}
+        for cat in yld_dict[proc].keys():
+            if cat not in cats_to_print: continue
+            yld_dict_for_printing[proc][cat] = (yld_dict[proc][cat],None)
+    mlt.print_latex_yield_table(
+        yld_dict_for_printing,
+        tag="All yields",
+        key_order=sample_dict_base.keys(),
+        subkey_order=cats_to_print,
+        print_begin_info=True,
+        print_end_info=True,
+        column_variable="subkeys",
+    )
 
 
 def main():
